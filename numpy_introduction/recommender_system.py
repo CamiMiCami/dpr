9a9b81a7f97e18cea77c112f50882a62af9f93b5
st180457 <st180457@stud.uni-stuttgart.de>
2024-04-07 23:29:27 +0200

    finish task 1

--- numpy_introduction/recommender_system.py
+++ numpy_introduction/recommender_system.py
@@ -16,10 +16,10 @@ class MatrixFactorization(object):
         self.lr = lr
 
         # Generate mask for known entries (non-zero elements), split the mask into a train and test mask
-        self.mask = # TODO (TASK 1)
-        self.split = # TODO (TASK 1)
-        self.mask_train = # TODO (TASK 1)
-        self.mask_test = # TODO (TASK 1)
+        self.mask = self.R.astype(int) # TODO (TASK 1)
+        self.split = np.random.rand(self.R.shape[0], self.R.shape[1]) # TODO (TASK 1)
+        self.mask_train = self.mask * (self.split > 0.2) # TODO (TASK 1)
+        self.mask_test = self.mask * (self.split < 0.2) # TODO (TASK 1)
         print(f"Known entries: {self.mask.sum()}, {self.mask_train.sum()} used for training and {self.mask_test.sum()} used for testing.")
 
         # Initialize low-rank user and movie matrix uniformly between 0 and 1
@@ -31,16 +31,16 @@ class MatrixFactorization(object):
         print(f"User matrix shape: {self.U.shape}, movie matrix shape: {self.V.shape}, total parameters: {total_parameters}")
 
     def gradient_user_matrix(self, error):
-        return # TODO (TASK 2)
+        return -2 * np.matmul((self.mask * error), self.V) # TODO (TASK 2)
 
     def gradient_movie_matrix(self, error):
-        return # TODO (TASK 2)
+        return -2 * np.matmul((self.mask * error).T, self.U) # TODO (TASK 2)
 
     def update_user_matrix(self, u_grad):
-        self.U = # TODO (TASK 3)
+        self.U = self.U - self.lr * u_grad # TODO (TASK 3)
 
     def update_movie_matrix(self, v_grad):
-        self.V = # TODO (TASK 3)
+        self.V = self.V - self.lr * v_grad # TODO (TASK 3)
 
     def rmse(self, split='all'):
         if split == 'train':
@@ -57,7 +57,7 @@ class MatrixFactorization(object):
             if i % 100 == 0:
                 print(f"Step {i}/{self.steps}, RMSE (train): {self.rmse('train'):.4f}, RMSE (test): {self.rmse('test'):.4f}")
 
-            error = # TODO (TASK 2) # Compute the error outside the gradient computation, so we don't have to do it twice
+            error = self.R - np.matmul(self.U, self.V.T) # TODO (TASK 2) # Compute the error outside the gradient computation, so we don't have to do it twice
             u_grad = self.gradient_user_matrix(error)
             v_grad = self.gradient_movie_matrix(error)
             self.update_user_matrix(u_grad)
