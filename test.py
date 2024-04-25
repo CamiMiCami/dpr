24a5706562dad46ee9ad50e08b8be0570d070aa4
st180457 <st180457@stud.uni-stuttgart.de>
2024-04-25 11:25:08 +0200

    test scripts

--- test.py
+++ test.py
@@ -10,6 +10,9 @@ size2 = math.ceil(size / 2)
 x1_banana1 = np.random.uniform(-2, 6, size1)
 x2_banana1 = -0.3 * (x1_banana1-2)**2 + 4 + 0.7 * np.random.randn(size1)
 
+x1_banana2 = np.random.uniform(-6, 2, size2)
+x2_banana2 = np.random.randn(size2)
+
 
 plt.scatter(x1_banana1, x2_banana1, color='blue', marker='o')
 
---
+++ test2.py
@@ -0,0 +1,10 @@
+import numpy as np
+import math
+
+size = 10
+
+size1 = math.floor(size / 2)
+size2 = math.ceil(size / 2)
+
+print(np.random.randn(size2))
+print(np.random.standard_normal(size2))
